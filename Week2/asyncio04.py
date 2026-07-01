d648cf9c7f0f22430e96d668340c99bec4e837e4
Pakorn <6710301037@cdti.ac.th>
2026-07-01 10:01:07 +0700

    Dosc:Updated asyncio 3,4,5

--- Week2/asyncio04.py
+++ Week2/asyncio04.py
@@ -1,2 +1,15 @@
 # Program 4: The await Keyword
 # Concept: Pausing a coroutine to let another operation finish using await.
+import asyncio
+from time import ctime
+
+async def main():
+    print(f"[{ctime()}] Task started")
+
+
+    await asyncio.sleep(1)  # Simulating a blocking operation
+
+    print(f"[{ctime()}] Task finished")
+
+if __name__ == "__main__":
+    asyncio.run(main())  # Running the main coroutine
--- Week2/asyncio05.py
+++ Week2/asyncio05.py
@@ -1,2 +1,21 @@
 # Program 5: Sequential Execution (The Wrong Way)
 # Concept: Showing that simply awaiting one after another is still sequential (Synchronous behavior).
+import asyncio
+from time import time,ctime
+
+async def sever_cistomer(name):
+    print(f"[{ctime()}] Cooking for {name}")
+    await asyncio.sleep(1)
+    print(f"{ctime()} Server for {name}")
+
+async def main():
+    start = time()
+
+    await sever_cistomer("A")
+    await sever_cistomer("B")
+
+    print(f"Total time: {time() - start:.2f} seconds")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())  # Running the main coroutine
